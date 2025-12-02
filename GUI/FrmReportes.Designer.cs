353fa5bd864a26b85f933c95b5c620f5eb41642a
Antony <antony6glam@gmail.com>
2025-12-02 15:58:39 -0500

    Agregado código de FrmReportes

---
+++ GUI/FrmReportes.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace GUI
+{
+    partial class FrmReportes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmReportes));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTipoReporte = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbLapso = new System.Windows.Forms.ComboBox();
+            this.dgvReportes = new System.Windows.Forms.DataGridView();
+            this.btnGenerarReporte = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTotalVentas = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReportes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(371, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "REPORTES VENTAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tipo de reporte: ";
+            // 
+            // cmbTipoReporte
+            // 
+            this.cmbTipoReporte.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbTipoReporte.FormattingEnabled = true;
+            this.cmbTipoReporte.Location = new System.Drawing.Point(254, 101);
+            this.cmbTipoReporte.Name = "cmbTipoReporte";
+            this.cmbTipoReporte.Size = new System.Drawing.Size(183, 37);
+            this.cmbTipoReporte.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(76, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Lapso: ";
+            // 
+            // cmbLapso
+            // 
+            this.cmbLapso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbLapso.FormattingEnabled = true;
+            this.cmbLapso.Location = new System.Drawing.Point(179, 182);
+            this.cmbLapso.Name = "cmbLapso";
+            this.cmbLapso.Size = new System.Drawing.Size(182, 33);
+            this.cmbLapso.TabIndex = 7;
+            // 
+            // dgvReportes
+            // 
+            this.dgvReportes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReportes.Location = new System.Drawing.Point(600, 101);
+            this.dgvReportes.Name = "dgvReportes";
+            this.dgvReportes.RowHeadersWidth = 51;
+            this.dgvReportes.RowTemplate.Height = 24;
+            this.dgvReportes.Size = new System.Drawing.Size(328, 439);
+            this.dgvReportes.TabIndex = 8;
+            // 
+            // btnGenerarReporte
+            // 
+            this.btnGenerarReporte.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnGenerarReporte.FlatAppearance.BorderSize = 0;
+            this.btnGenerarReporte.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.btnGenerarReporte.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerarReporte.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarReporte.ForeColor = System.Drawing.Color.White;
+            this.btnGenerarReporte.Image = ((System.Drawing.Image)(resources.GetObject("btnGenerarReporte.Image")));
+            this.btnGenerarReporte.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGenerarReporte.Location = new System.Drawing.Point(81, 276);
+            this.btnGenerarReporte.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGenerarReporte.Name = "btnGenerarReporte";
+            this.btnGenerarReporte.Size = new System.Drawing.Size(319, 62);
+            this.btnGenerarReporte.TabIndex = 13;
+            this.btnGenerarReporte.Text = "GENERAR";
+            this.btnGenerarReporte.UseVisualStyleBackColor = false;
+            this.btnGenerarReporte.Click += new System.EventHandler(this.btnGenerarReporte_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(120, 561);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Total de ventas: ";
+            // 
+            // lblTotalVentas
+            // 
+            this.lblTotalVentas.AutoSize = true;
+            this.lblTotalVentas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalVentas.Location = new System.Drawing.Point(300, 561);
+            this.lblTotalVentas.Name = "lblTotalVentas";
+            this.lblTotalVentas.Size = new System.Drawing.Size(90, 25);
+            this.lblTotalVentas.TabIndex = 15;
+            this.lblTotalVentas.Text = "$ XXXX";
+            // 
+            // FrmReportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 624);
+            this.Controls.Add(this.lblTotalVentas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnGenerarReporte);
+            this.Controls.Add(this.dgvReportes);
+            this.Controls.Add(this.cmbLapso);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbTipoReporte);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmReportes";
+            this.Text = "FrmReportes";
+            this.Load += new System.EventHandler(this.FrmReportes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReportes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbTipoReporte;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbLapso;
+        private System.Windows.Forms.DataGridView dgvReportes;
+        private System.Windows.Forms.Button btnGenerarReporte;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTotalVentas;
+    }
+}
---
+++ GUI/FrmReportes.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using GUI.Usuarios;  // VentaService y VentaRepository
+using GUI.Producto;  // VentaC
+
+namespace GUI
+{
+    public partial class FrmReportes : Form
+    {
+        private readonly VentaService _ventaService;
+
+        public FrmReportes()
+        {
+            InitializeComponent();
+
+            // Conexión a tu base de datos real
+            _ventaService = ServiceFactory.CrearVentaService();
+
+        }
+
+        private void FrmReportes_Load(object sender, EventArgs e)
+        {
+            cmbTipoReporte.Items.AddRange(new[] { "Ventas por día", "Más vendidos", "Totales" });
+            cmbLapso.Items.AddRange(new[] { "Hoy", "3 días", "7 días", "15 días", "30 días", "Todos" });
+
+            cmbTipoReporte.SelectedIndex = 0;
+            cmbLapso.SelectedIndex = 0;
+        }
+
+
+        private void btnGenerarReporte_Click(object sender, EventArgs e)
+        {
+            if (cmbTipoReporte.SelectedItem == null || cmbLapso.SelectedItem == null)
+            {
+                MessageBox.Show("Seleccione un tipo de reporte y un lapso de tiempo.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string tipo = cmbTipoReporte.SelectedItem.ToString();
+            string lapso = cmbLapso.SelectedItem.ToString();
+
+            var ventas = _ventaService.Listar().ToList();
+            DateTime desde = CalcularFechaDesde(lapso);
+            var ventasFiltradas = ventas.Where(v => v.Fecha >= desde).ToList();
+
+            switch (tipo)
+            {
+                case "Ventas por día":
+                    GenerarVentasPorDia(ventasFiltradas);
+                    break;
+                case "Más vendidos":
+                    GenerarMasVendidos(ventasFiltradas);
+                    break;
+                case "Totales":
+                    GenerarTotales(ventasFiltradas);
+                    break;
+            }
+        }
+
+        private DateTime CalcularFechaDesde(string lapso)
+        {
+            int dias = 0;
+
+            if (lapso == "Hoy")
+                dias = 0;
+            else if (lapso == "3 días")
+                dias = 3;
+            else if (lapso == "7 días")
+                dias = 7;
+            else if (lapso == "15 días")
+                dias = 15;
+            else if (lapso == "30 días")
+                dias = 30;
+            else if (lapso == "Todos")
+                return DateTime.MinValue; // incluye todas las ventas
+
+            return DateTime.Now.AddDays(-dias);
+        }
+
+
+
+        private void GenerarVentasPorDia(List<VentaC> ventas)
+        {
+            var reporte = ventas
+                .GroupBy(v => v.Fecha.Date)
+                .Select(g => new
+                {
+                    Fecha = g.Key.ToString("dd/MM/yyyy"),
+                    TotalVentas = g.Sum(v => v.Total)
+                })
+                .OrderBy(r => r.Fecha)
+                .ToList();
+
+            dgvReportes.DataSource = reporte;
+            lblTotalVentas.Text = "$ " + reporte.Sum(r => r.TotalVentas).ToString("N2");
+        }
+
+        private void GenerarMasVendidos(List<VentaC> ventas)
+        {
+            // Suponiendo que ListaProductos guarda nombres separados por coma
+            var productos = ventas
+                .SelectMany(v => v.ListaProductos.Split(','))
+                .Select(p => p.Trim())
+                .Where(p => !string.IsNullOrEmpty(p))
+                .GroupBy(p => p)
+                .Select(g => new
+                {
+                    Producto = g.Key,
+                    CantidadVendida = g.Count()
+                })
+                .OrderByDescending(r => r.CantidadVendida)
+                .ToList();
+
+            dgvReportes.DataSource = productos;
+            lblTotalVentas.Text = "$ " + ventas.Sum(v => v.Total).ToString("N2");
+        }
+
+        private void GenerarTotales(List<VentaC> ventas)
+        {
+            int totalRegistros = ventas.Count;
+            decimal totalVentas = ventas.Sum(v => v.Total);
+
+            var resumen = new[]
+            {
+                new { Descripción = "Total de ventas registradas", Total = totalRegistros.ToString() },
+                new { Descripción = "Monto total vendido", Total = totalVentas.ToString("N2") }
+            };
+
+            dgvReportes.DataSource = resumen;
+            lblTotalVentas.Text = "$ " + totalVentas.ToString("N2");
+        }
+    }
+}
